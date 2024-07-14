595c6f04af83d6f6ac52ff0997035c8795b38ef5
da-verge <vterrel00@gmail.com>
2024-07-14 09:12:27 -0700

    added randomly generated tiles which will become obstacles later

--- rogue/runner.py
+++ rogue/runner.py
@@ -1,4 +1,6 @@
+import random
 import pygame.time
+
 
 class Board:
     BOARD_HEIGHT = 600
@@ -6,13 +8,18 @@ class Board:
     TILE_HEIGHT = 50
     TILE_WIDTH = 50
     MIN_X_TILE = 1
-    MAX_X_TILE = BOARD_HEIGHT / TILE_HEIGHT - 2
-    MAX_Y_TILE = BOARD_WIDTH / TILE_WIDTH - 2
+    MAX_X_TILE = BOARD_HEIGHT // TILE_HEIGHT - 2
+    MAX_Y_TILE = BOARD_WIDTH // TILE_WIDTH - 2
     MIN_Y_TILE = 1
     BORDERS = (0, 100, 100)
 
+    def __init__(self):
+        self.obstacles = [(random.randint(2, self.MAX_X_TILE), random.randint(2, self.MAX_Y_TILE))
+                          for i in range(10)]
+
     def draw(self):
         self.draw_board()
+        self.draw_obstacles()
 
     def draw_board(self):
         for i in range(self.TILE_WIDTH, self.BOARD_HEIGHT, self.TILE_WIDTH):
@@ -20,16 +27,24 @@ class Board:
         for i in range(self.TILE_HEIGHT, self.BOARD_WIDTH, self.TILE_HEIGHT):
             screen.draw.line((i, 0), (i, self.BOARD_HEIGHT), self.BORDERS)
 
+    def draw_obstacles(self):
+        for obstacle in self.obstacles:
+            m = Rect((obstacle[0] * self.TILE_WIDTH, 
+                      obstacle[1] * self.TILE_HEIGHT), 
+                     (self.TILE_WIDTH, self.TILE_HEIGHT))
+            screen.draw.filled_rect(m, (0, 200, 0))
+
 class Character:
-    COLOR_OF_CHARACTER = ( 200, 0, 0)
+    COLOR= ( 200, 0, 0)
 
     def __init__(self):
         self.x = 0
         self.y = 0
 
     def draw(self, board,):
-        r = Rect((self.y * board.TILE_WIDTH, self.x * board.TILE_HEIGHT), (board.TILE_WIDTH, board.TILE_HEIGHT))
-        screen.draw.filled_rect(r, Character.COLOR_OF_CHARACTER)
+        r = Rect((self.y * board.TILE_WIDTH, self.x * board.TILE_HEIGHT), 
+                 (board.TILE_WIDTH, board.TILE_HEIGHT))
+        screen.draw.filled_rect(r, Character.COLOR)
 
 
 class World:
@@ -61,16 +76,32 @@ def draw():
     world.draw()
     
 
+def collide():
+    global world
+    if get_color(0,200,0) == True:
+        collision = True
+
+
 def move():
     global world
+    collision = False
     if keyboard.w:
         world.move_character(-1, 0)
+        if collision == True:
+            world.move_character(1, 0)
     if keyboard.s:
         world.move_character(1, 0)
+        if collision == True:
+            world.move_character(-1, 0)
     if keyboard.a:
         world.move_character(0, -1)
+        if collision == True:
+            world.move_character(0, 1)
     if keyboard.d:
         world.move_character(0, 1)
+        if collision == True:
+            world.move_character(0, -1)
+
 
 def update():
     move()
